61933d5620380f7ef7d7c0f5cc4c64f764cdd903
racka98 <bmkilaha@gmail.com>
2022-09-05 18:43:12 +0300

    switch server provider to render.com

--- common/network/src/commonMain/kotlin/work.racka.thinkrchive.v2.common.network/util/Constants.kt
+++ common/network/src/commonMain/kotlin/work.racka.thinkrchive.v2.common.network/util/Constants.kt
@@ -1,6 +1,6 @@
 package work.racka.thinkrchive.v2.common.network.util
 
 internal object Constants {
-    private const val BASE_URL = "https://thinkrchive-server.herokuapp.com/v1"
+    private const val BASE_URL = "https://thinkrchive-server.onrender.com/v1"
     const val ALL_LAPTOPS = "$BASE_URL/all-laptops"
 }
